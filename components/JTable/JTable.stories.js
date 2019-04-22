7139226ff7d649ee2206007d6869c2175e5c6e40
José Silva <jose.silva@jumia.com>
2019-04-22 09:06:01 +0100

    JTable component

---
+++ components/JTable/JTable.stories.js
@@ -0,0 +1,27 @@
+import { storiesOf } from '@storybook/vue'
+import JTable from './JTable'
+
+var items = [
+  [ 'Yair Lamb', 'yair.lamb@email.com' ],
+  [ 'Leonardo Payne', 'leonardo.payne@email.com' ],
+  [ 'Carl Henson', 'carl.henson@email.com' ],
+  [ 'Jensen Combs', 'jensen.combs@email.com' ],
+  [ 'Amiah Burton', 'amiah.burton@email.com' ],
+  [ 'Yaretzi Mayo', 'yaretzi.mayo@email.com' ],
+  [ 'Kamren Huffman', 'kamren.huffman@email.com' ]
+]
+
+storiesOf('JTable', module)
+  .add('with 7 items', () => ({
+    components: { JTable },
+    template:
+    `
+      <JTable
+        :items="items"
+      />
+    `,
+    data: () => ({
+      items
+    })
+  }))
+  
---
+++ components/JTable/JTable.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <table>
+    <JTableRow
+      v-for="(values, index) in items"
+      :key="index"
+      :values="values"
+    />
+  </table>
+</template>
+
+<script>
+import JTableRow from '../JTableRow/JTableRow'
+export default {
+  name: 'JTable',
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  components: {
+    JTableRow
+  }
+}
+</script>
